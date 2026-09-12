7060b8bb3e553a35a97c9454971575ec78248378
NHDuong2612 <24020104@vnu.edu.vn>
2026-09-12 14:26:50 +0700

    task6

---
+++ week01/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, request   
+from uuid import uuid4
+app = Flask(__name__)
+app.json.sort_keys = False
+_next = 1
+BOOKS = [{"id":1,"title":"Clean Code","author":"R. Martin"}]
+def find_by_id(book_id):
+    return next((b for b in BOOKS if b["id"] == book_id), None)
+
+# LIST - GET /books
+@app.route("/books", methods=["GET"])
+def list_books():
+    limit=int(request.args.get("limit", 100))
+    return jsonify(BOOKS[:limit]), 200
+
+# DETAIL - GET /books/<book_id>
+@app.route("/books/<int:book_id>", methods=["GET"])
+def get_book(book_id):
+    book = find_by_id(book_id)
+    if book is None:
+        return jsonify({"error": "Book khong tim thay"}), 404
+    return jsonify(book), 200
+@app.route("/items/<int:item_id>", methods=["GET"])
+def get_item(item_id):
+    return jsonify({"id": item_id}), 200
+
+# CREATE - POST /books
+@app.route("/books", methods=["POST"])
+def create_book():
+    global _next
+    _next+=1
+    body = request.get_json(silent=True) or {}
+    title = body.get("title", "")
+    author = body.get("author", "")
+    if not title or not author:
+        return jsonify({"error": "Title va author la bat buoc"}), 400
+    book = {"id": _next, "title": title, "author":body.get("author","") }
+    BOOKS.append(book)
+    return jsonify(book), 201, {"Location": f"/books/{book['id']}"}
+
+# UPDATE - PUT /books/<book_id>
+@app.route("/books/<int:book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = find_by_id(book_id)
+    if not book:
+        return jsonify({"error": "not fount"}), 404
+    data = request.get_json(silent=True) or {}
+
+    print("DATA NHAN DUOC:", data)
+
+    book.update(data or {})
+
+    print("BOOK SAU KHI UPDATE:", book)
+    return jsonify(book), 200
+
+# DELETE - DELETE /books/<book_id>
+@app.route("/books/<int:book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = find_by_id(book_id)
+    if not book:
+        return jsonify({"error": "not fount"}), 404
+    BOOKS.remove(book)
+    return "", 200
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001, debug=True)
+
+
+               
