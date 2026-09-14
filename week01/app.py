9ee421ef5cb221e4bb071797c85090ed8f54a613
NHDuong2612 <24020104@vnu.edu.vn>
2026-09-14 14:28:07 +0700

    task6btvn

--- week01/app.py
+++ week01/app.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 app = Flask(__name__)
 app.json.sort_keys = False
 _next = 1
-BOOKS = [{"id":1,"title":"Clean Code","author":"R. Martin"}]
+BOOKS = [{"id":1,"title":"Clean Code","author":"R. Martin","year":2008}]
 def find_by_id(book_id):
     return next((b for b in BOOKS if b["id"] == book_id), None)
 
@@ -11,7 +11,15 @@ def find_by_id(book_id):
 @app.route("/books", methods=["GET"])
 def list_books():
     limit=int(request.args.get("limit", 100))
-    return jsonify(BOOKS[:limit]), 200
+    q = request.args.get("q")
+    sort = request.args.get("sort")
+    if q:
+        result = [book for book in BOOKS if q.lower() in book["title"].lower()]
+    else:
+        result = BOOKS
+    if sort == "title":
+        result = sorted(result, key=lambda book: book["title"])
+    return jsonify(result), 200
 
 # DETAIL - GET /books/<book_id>
 @app.route("/books/<int:book_id>", methods=["GET"])
@@ -28,13 +36,16 @@ def get_item(item_id):
 @app.route("/books", methods=["POST"])
 def create_book():
     global _next
-    _next+=1
     body = request.get_json(silent=True) or {}
     title = body.get("title", "")
     author = body.get("author", "")
-    if not title or not author:
-        return jsonify({"error": "Title va author la bat buoc"}), 400
-    book = {"id": _next, "title": title, "author":body.get("author","") }
+    year = body.get("year","")
+    if not title or not author or not year:
+        return jsonify({"error": "Title, author va year la bat buoc"}), 400
+    if year < 1900:
+        return jsonify({"error":"year phai >=1900"})
+    _next+=1
+    book = {"id": _next, "title": title, "author":body.get("author",""),"year":body.get("year", 2023)}
     BOOKS.append(book)
     return jsonify(book), 201, {"Location": f"/books/{book['id']}"}
 
@@ -45,12 +56,7 @@ def update_book(book_id):
     if not book:
         return jsonify({"error": "not fount"}), 404
     data = request.get_json(silent=True) or {}
-
-    print("DATA NHAN DUOC:", data)
-
     book.update(data or {})
-
-    print("BOOK SAU KHI UPDATE:", book)
     return jsonify(book), 200
 
 # DELETE - DELETE /books/<book_id>
@@ -61,6 +67,7 @@ def delete_book(book_id):
         return jsonify({"error": "not fount"}), 404
     BOOKS.remove(book)
     return "", 200
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5001, debug=True)
